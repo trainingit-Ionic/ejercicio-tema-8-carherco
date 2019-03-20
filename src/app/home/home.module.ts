b2598e2d00568905ef7dd37d4a6df9facd320bd8
Carlos Herrera Conejero <carherco@gmail.com>
2019-03-20 16:06:00 +0100

    ejercicio resuelto

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
+import { Flashlight } from '@ionic-native/flashlight/ngx';
 
 @NgModule({
   imports: [
@@ -18,6 +19,7 @@ import { HomePage } from './home.page';
       }
     ])
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  providers: [Flashlight]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
+import { Platform } from '@ionic/angular';
 import { Component } from '@angular/core';
+import { Flashlight } from '@ionic-native/flashlight/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +9,41 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  ready = false;
+  error = false;
+
+  constructor(private flashlight: Flashlight, private platform: Platform) { 
+    this.platform.ready()
+    .then(
+      () => this.platform.is('cordova')
+    )
+    .then(
+      isCordova => {
+        if(isCordova){
+          return this.flashlight.available()
+        } else {
+          return Promise.reject('No es cordoba');
+        }
+      }
+    )
+    .then(
+      isAvailable => {
+        if(isAvailable){
+          return Promise.resolve(true);
+        } else {
+          return Promise.reject('No hay flash');
+        }
+      }
+    )
+    .then(
+      () => this.ready = true
+    )
+    .catch(
+      () => this.error = true
+    );
+  }
+
+  toggleFlash() {
+    this.flashlight.toggle();
+  }
 }
